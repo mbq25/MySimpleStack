445910fdd8fea07b573195f7e8e5c50172412572
mbq25 <morgane.bouquard@gmail.com>
2015-03-08 14:37:27 +0100

    Add Item class and SimpleStack Interface.

---
+++ src/main/java/dcll/mbou/mysimplestack/Item.java
@@ -0,0 +1,20 @@
+package dcll.mbou.mysimplestack;
+
+/**
+ * Created by Morgane on 08/03/2015.
+ */
+public class Item {
+    private Object obj;
+
+    public Item(Object obj) {
+        this.obj = obj;
+    }
+
+    public Object getObj() {
+        return obj;
+    }
+
+    public void setObj(Object obj) {
+        this.obj = obj;
+    }
+}
